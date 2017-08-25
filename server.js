619b4c8c8c1cce28de894cf42c69cc2ee1303218
overpowerradio <rhythms.promise@gmail.com>
2017-08-25 14:41:54 -0400

    debug server

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var passport = require('passport');
 var session = require('express-session');
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var exphbs = require('express-handlebars');
 var path = require("path");
 
-// bring in the models
+// The models
 var db = require("./models");
 
-//For BodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/overcome_controller.js");
+var authRoute = require('./routes/auth.js')(app, passport);
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
 // For Passport
 app.use(session({ secret: 'team EMS', resave: true, saveUninitialized: true })); // session secret
@@ -19,25 +35,12 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 require('./config/passport/passport.js')(passport, db.User);
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
-
-//For Handlebars
-app.set('views', './views')
-app.engine('handlebars', exphbs({ extname: '.handlebars' }));
-app.set('view engine', '.handlebars');
-
-//Routes
-var routes = require("./controllers/overcome_controller.js");
-app.use("./", routes);
-app.use("/update", routes);
-app.use("/create", routes);
-
-var authRoute = require('./routes/auth.js')(app, passport);
-
+// listen on port 3000
 var port = process.env.PORT || 3000;
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(port);
+  console.log("You are listening to port: " + port);
 });
+
 
 console.log(module.exports);
